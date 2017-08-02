1c01318385177dd98adb81ecc617d2acfcef1dea
binitabaiju <binitabaiju@gmail.com>
2017-08-02 10:42:39 +0545

    second commit

--- HalftoneDesignJava/src/bankapp/Main.java
+++ HalftoneDesignJava/src/bankapp/Main.java
@@ -1,6 +1,7 @@
 package bankapp;
 
 public class Main {
+	//this is my second commit
 	public static void main(String [] args){
 		BankAccount bank=new BankAccount("Binita","02AB5");
 		bank.showMenu();
